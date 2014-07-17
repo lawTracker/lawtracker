4f35655eb18557ca9e5334ba40b147606e1d4feb
BorisVerk <boris.verk@gmail.com>
2014-07-17 10:52:00 -0600

    implemented simple authentication view

---
+++ client/app/auth/auth.js
@@ -0,0 +1,16 @@
+angular.module('lawtracker.auth', [])
+
+.controller('AuthController', function ($scope, $location) {
+  $scope.user = {};
+
+  $scope.signin = function () {
+    console.log($scope.user) //sign in through git lab here
+    $location.path('/bills');
+  };
+
+  $scope.signup = function () {
+    console.log($scope.user) //sign up through git lab here
+
+    $location.path('/bills');
+  };
+});
