7799436ea6dd33027dbb02d6eac54739742f0e03
taojonesin <taojonesin (at) nowhere.com>
2014-07-20 21:43:45 -0700

    changed post url for repository creation

--- client/js/services.js
+++ client/js/services.js
@@ -4,7 +4,7 @@ lawTrackerServices.factory('Repository', function($http) {
   var createRepository = function(user, repoName) {
     return $http({
       method: 'POST',
-      url: '/api/repositories',
+      url: '/api/repositories/create',
       data: JSON.stringify({user: user, repositoryName: repoName})
     });
   };
