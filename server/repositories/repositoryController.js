fc4fcedc49aa16830232af67ea09ccff29b768a1
jhreact <jhreact@users.noreply.github.com>
2014-07-23 20:34:40 -0700

    routing testing works for both get and post...w00t!

--- server/repositories/repositoryController.js
+++ server/repositories/repositoryController.js
@@ -1,9 +1,13 @@
 var fs = require('fs');
+var gitlab = require('node-gitlab');
 
 module.exports = {
   create: function (req, res, next) {
     console.log('In create reposistory');
     var repositoryName = req.body.repository;
+  },
+  test: function (req, res, next) {
+    console.log('Testing, testing, 1, 2, 3...');
   }
 
 };
--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/../client"))
 
+require('./config/middleware.js')(app, express);
+module.exports = app;
+
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function(req, res) {
   console.log('listening on port %d', server.address().port);
